d053aa73b6c88d2a1ade816485318dfc8957e42d
Novanna Rahma Zani <novannarahmazani@gmail.com>
2021-11-10 20:12:52 +0700

    commit

--- Asset/js/generator.js
+++ Asset/js/generator.js
@@ -1,18 +1,61 @@
-<script>
-function cardGenerator(){
- let userNamel = document.getElementById("namaL");
- let userNamep = document.getElementById("namaP");
- let userBirthplace = document.getElementById("birthplace");
- let userBod = document.getElementById("bod");
- let userRelg = document.getElementByName("agama");
- let userGender = document.getElemetByName("gender");
- let userGender = document.getElementById("domisili");
- let userEduc = document.getElementByName("pendidikan");
- let userSchname = document.getElementById("schoolName");
- let userProglang = document.getElementByName("proglang");
- let whatsappNum = document.getElementById("wa-num");
- let userMail = document.getElementById("mail");
+let openModalTrigger = document.getElementById("button");
 
- alert(userNamel);
+openModalTrigger.addEventListener("click", openModal);
+
+function openModal() {
+  document.getElementById("modal-wrapper").classList.add("visible");
+
+  generatePreview();
 }
-</script>
+
+function generatePreview(){
+    let userNamel = document.getElementById("namaL").value;
+    let userNamep = document.getElementById("namaP").value;
+    let userBirthplace = document.getElementById("birthplace").value;
+    let userBod = document.getElementById("bod").value;
+    let userRelg = document.getElementByName("agama").value;
+    let userGender = document.getElemetByName("gender").value;
+    let userLoc = document.getElementById("domisili").value;
+    let userEduc = document.getElementByName("pendidikan").value;
+    let userSchname = document.getElementById("schoolName").value;
+    let userProglang = document.getElementByName("proglang").value;
+    let userWanum = document.getElementById("wa-num").value;
+    let userMail = document.getElementById("mail").value;
+    let userAva = document.getElementById("ava").value;
+
+    let cardNamel = document.getElementById("previewNamel");
+    let cardNamep = document.getElementById("previewNamep");
+    let cardBirthplace = document.getElementById("previewBirthplace");
+    let cardBod = document.getElementById("previewBod");
+    let cardRelg = document.getElementById("previewRelg");
+    let cardGender = document.getElementById("previewGender");
+    let cardLoc = document.getElementById("previewLoc");
+    let cardEduc = document.getElementById("previewEduc");
+    let cardSchname = document.getElementById("previewSchname");
+    let cardProglang = document.getElementById("previewProglang");
+    let cardWanum = document.getElementById("previewWanum");
+    let cardMail = document.getElementById("previewMail");
+    let cardAva = document.getElementById("previewAva");
+
+    cardNamel.innerHTML = nameLInput;
+    cardNamep.innerHTML = namePInput;
+    cardBirthplace.innerHTML = birthPlaceInput;
+    cardBod.innerHTML = bodInput;
+    cardRelg.innerHTML = relgInput;
+    cardGender.innerHTML = genderInput;
+    cardLoc.innerHTML = locInput;
+    cardEduc.innerHTML = educInput;
+    cardSchname.innerHTML = schnameInput;
+    cardProglang.innerHTML = proglangLInput;
+    cardWanum.innerHTML = wanumInput;
+    cardMail.innerHTML = mailInput;
+    cardAva.setAttribute("src","img/" + userAva.split("\\").pop());
+
+}
+
+let modalWindow = document.getElementById("modal-wrapper");
+modalWindow.addEventListener("click", closeModal);
+
+function closeModal() {
+  modalWindow.classList.remove("visible");
+}
