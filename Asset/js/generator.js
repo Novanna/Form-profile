c748a5afd1ae6ead6f5b9d17a70261526bf70da1
Novanna Rahma Zani <novannarahmazani@gmail.com>
2021-11-10 14:55:31 +0700

    commit

--- Asset/js/generator.js
+++ Asset/js/generator.js
@@ -1,3 +1,18 @@
+<script>
 function cardGenerator(){
-    
+ let userNamel = document.getElementById("namaL");
+ let userNamep = document.getElementById("namaP");
+ let userBirthplace = document.getElementById("birthplace");
+ let userBod = document.getElementById("bod");
+ let userRelg = document.getElementByName("agama");
+ let userGender = document.getElemetByName("gender");
+ let userGender = document.getElementById("domisili");
+ let userEduc = document.getElementByName("pendidikan");
+ let userSchname = document.getElementById("schoolName");
+ let userProglang = document.getElementByName("proglang");
+ let whatsappNum = document.getElementById("wa-num");
+ let userMail = document.getElementById("mail");
+
+ alert(userNamel);
 }
+</script>
